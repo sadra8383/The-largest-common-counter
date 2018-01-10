6bbef752376a4c5509c9a10b3d3beb7e90fd042c
sadra8383 <35002939+sadra8383@users.noreply.github.com>
2018-01-10 17:09:43 +0330

    The codes
    nothing

---
+++ 2.py
@@ -0,0 +1,18 @@
+n=int(input())
+h=int(input())
+o=n%h
+s=h
+if o!=0:
+    while s%o!=0:
+        if s%o==0:
+           s%o==0 
+        else:
+            s=o
+            o=h%o
+            if s%o==0:
+                s%o==0
+print(o)
+print("if it print 0 it means your smaler number is the answer")
+
+        
+    
